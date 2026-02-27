e131b2504f3edf0a0b1475f94a29a42907647583
nathaliaif <nathalia.iro.fernandes@gmail.com>
2026-02-27 19:39:29 -0300

    feat: add Atkinson Hyperlegible font to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { StackProvider, StackTheme } from "@stackframe/stack";
 import { stackClientApp } from "../stack/client";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+import { AppProviders } from "@/presentation/providers";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +16,33 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const atkinson = localFont({
+  src: [
+    {
+      path: "../public/fonts/AtkinsonHyperlegible-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/AtkinsonHyperlegible-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/AtkinsonHyperlegible-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../public/fonts/AtkinsonHyperlegible-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-atkinson",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -27,12 +56,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={inter.variable}>
+    <html lang="pt-BR" className={`${inter.variable} ${atkinson.variable}`}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ><StackProvider app={stackClientApp} lang="pt-BR"><StackTheme>
-        {children}
-      </StackTheme></StackProvider></body>
+      >
+        <StackProvider app={stackClientApp} lang="pt-BR">
+          <StackTheme>
+            <AppProviders>{children}</AppProviders>
+          </StackTheme>
+        </StackProvider>
+      </body>
     </html>
   );
 }
